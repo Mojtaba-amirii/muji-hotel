8900039ec1879411a86a85cf0646248871e05497
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2025-05-03 10:26:58 +0200

    chore: update dependencies and devDependencies in package.json
    - Updated @sanity/client to ^7.0.0
    - Updated @sanity/vision to ^3.87.1
    - Updated @stripe/stripe-js to ^7.3.0
    - Updated next-sanity to ^9.10.6
    - Updated sanity to ^3.87.1
    - Updated stripe to ^18.1.0
    - Updated @tailwindcss/postcss to ^4.1.5
    - Updated @types/react-dom to ^19.1.3
    - Updated eslint to ^9.26.0
    - Updated tailwindcss to ^4.1.5

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -6,7 +6,7 @@ import { authOptions } from "@/libs/auth";
 import { getRoom } from "@/libs/apis";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2025-04-30.basil",
 });
 
 type RequestData = {
--- app/api/webHook/route.ts
+++ app/api/webHook/route.ts
@@ -1,12 +1,12 @@
-import { NextResponse, NextRequest } from "next/server";
 import Stripe from "stripe";
+import { NextResponse, NextRequest } from "next/server";
 
 import { createBooking, updateHotelRoom } from "@/libs/apis";
 
 const checkout_session_completed = "checkout.session.completed";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2025-04-30.basil",
 });
 
 export async function POST(req: NextRequest) {
