1602fcffafe7dd434f1bd60e4f9f00e4f528bd2d
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2025-05-29 11:01:14 +0200

    chore: update dependencies in package.json
    - bump stripe from 18.1.1 to 18.2.0
    - bump @tailwindcss/postcss from 4.1.7 to 4.1.8
    - bump @types/node from 22.15.23 to 22.15.24
    - bump postcss from 8.5.2 to 8.5.4
    - bump tailwindcss from 4.1.7 to 4.1.8

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -6,7 +6,7 @@ import { authOptions } from "@/libs/auth";
 import { getRoom } from "@/libs/apis";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2025-05-28.basil",
 });
 
 type RequestData = {
